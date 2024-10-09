7a723a5b7bcee41a57c2922e6db8a7c872b5af1b
ROHIT KUMAR YADAV <130483396+rohitzadavv@users.noreply.github.com>
2024-10-09 21:19:57 +0000

    date and time

---
+++ basics/date.js
@@ -0,0 +1,22 @@
+// let date = new Date();
+// console.log(date.getHours());//// 20
+
+// console.log(date.toDateString());//Wed Oct 09 2024
+
+// console.log(date.toLocaleDateString());//10/9/2024
+// console.log(date.toISOString());//2024-10-09T20:46:55.615Z
+
+// console.log(date.toLocaleTimeString());//8:46:55 PM
+
+// console.log(date.getTimezoneOffset());// 0
+// console.log(date.toLocaleDateString());//10/9/2024
+
+
+
+// let mycreateddate =  new Date("10-04-2003");
+// console.log(mycreateddate);
+// let mytime = Date.now();// it wil give the value in millisecond
+// console.log(mytime);
+// console.log(Math.floor((mytime/1000)/3600))
+// console.log(mytime);
+
