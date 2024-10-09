e292eca8a6a34d4baf70b418e482b45cbe5d8e70
ROHIT KUMAR YADAV <130483396+rohitzadavv@users.noreply.github.com>
2024-10-09 20:23:24 +0000

    nums_and_math

---
+++ basics/conversion.js
@@ -0,0 +1,36 @@
+// let score = undefined;
+// let changeInNumber = Number(score);
+// console.log(typeof changeInNumber)
+// console.log( typeof score);
+// console.log(changeInNumber)
+
+// 3 => 3  number chnage into number easily
+// let value = 3;
+// let negative = -(value)
+// console.log(value);
+// console.log(negative);
+// let power = (2**3);
+// console.log(power)
+ 
+
+// let name = " rohit  kuamr yadav";
+// let repoCount = 76;
+// console.log(` My name is ${name} , my repcount is ${repoCount}`);// another  ways to print the  string 
+
+// const name1 = new String("rohit");
+// console.log(name1[0]);
+// console.log(name1.proto);
+// console.log(name1.length);
+// console.log(name1.toUpperCase());
+// console.log(name.substring(1,4))
+// const url = "https://rohitkumar%20yadav";
+// const clean = url.replace("%20","hii");
+// console.log(url);
+// console.log(clean);
+ const name = "rohit-kumar-yadav";
+ console.log(name.split('-'));
+
+
+
+
+
---
+++ basics/nums_and_math.js
@@ -0,0 +1,32 @@
+// let acoount_number = 100000;
+// let account_id = new Number(100000);// specially show tha aacount  id is the number data types
+// console.log(account_id);
+// console.log(acoount_number)
+// account_id.toFixed(7);
+// console.log(account_id);
+// let num = 123.456789;
+// console.log(num.toFixed(2)); // Outputs: "123.46"
+// console.log(num.toFixed(0)); // Outputs: "123"
+// console.log(num.toFixed(5)); // Outputs: "123.45679"
+// console.log(account_id.valueOf());
+  
+
+// let number = 1000000000;
+// console.log(number.toFixed(2));
+
+// console.log(number.toPrecision(3));
+
+// console.log(number.toLocaleString('en-IN'));// en-IN change united atndird value to indian stnadird value
+
+
+//+++++++++++++++++++++++++++math++++++++++++++++++++++++++++++++++++++
+console.log(Math.random());
+console.log(Math.min(1,2,3,4,5))
+console.log(Math.max(1,2,3,4,5,6))
+console.log(Math.floor(4.9))
+console.log(Math.ceil(6.7))
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
