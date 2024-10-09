5ce2987a25e95593a2e925ffa47873144923ed29
ROHIT KUMAR YADAV <130483396+rohitzadavv@users.noreply.github.com>
2024-10-09 13:45:55 +0000

    datatypes

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -2,3 +2,15 @@
 
 //alert( 3+ 3) showing errror beause we are using  node not browser
 
+let name = "rohit"
+let isloggedIn = true;
+let age =  21; 
+let state;
+// number =>  2 to the power 52
+//biginit
+//string
+ // null=> stand alone value 
+ // undefined means which is not defined
+ console.log(typeof age)
+ console.log(typeof undefined )
+ console.log(typeof null)
