bb7d5aa446e91bc2dbaa8476a93b4f2250d4cb22
ROHIT KUMAR YADAV <130483396+rohitzadavv@users.noreply.github.com>
2024-10-20 12:03:06 +0000

    object

--- basics/array.js
+++ basics/array.js
@@ -9,11 +9,32 @@
 // array.push(6)
 // array.push(6)
 // console.log(array)
-let arr1 = [0,1,2,3,4,5];
-console.log(arr1.includes(0));
-let slice = arr1.slice(0,4)
-console.log(slice)
-console.log("A", arr1)
-let splice = arr1.splice(0,3) // modify the original array
-console.log("B",arr1) // only rest of part will be in the array
-console.log(splice);
+//let arr1 = [0,1,2,3,4,5];
+// console.log(arr1.includes(0));
+// let slice = arr1.slice(0,4)
+// console.log(slice)
+// console.log("A", arr1)
+// let splice = arr1.splice(0,3) // modify the original array
+// console.log("B",arr1) // only rest of part will be in the array
+// console.log(splice);
+
+// let arr = ["ram ", "aam","shyam", "khata", "hai"]
+// let arr2 = ["delhi", "Mubae", "India","pakistan"]
+// arr.push(arr2);
+// // console.log(arr);
+// // console.log( " ye hai fifth element",arr[5])
+//  let newarray = arr.concat(arr2);
+// console.log("new array", newarray);
+
+
+// let first = [12,3,4,5,6,7]
+// let second = [10,23,45,5,50];
+// let newarray = first.concat(second);
+// console.log(newarray)
+
+//-------------------------------------------------/spread operator----
+
+let first = [10,20,30,40]
+let second = [50,60,70,70]
+let final = [...first,...second];
+console.log(final)
---
+++ basics/object.js
@@ -0,0 +1,33 @@
+// const user = {
+//     name :"Rohit",
+//     age:33,
+//     "hello duniya wale"
+//     school:"DAV Public School",
+//     email:"rohitkumaryadavv@gmail.com",
+//     isloggedin:"true"
+// }
+// console.log(user)
+// console.log(user["email"])
+
+// const person = {
+//     name: "mohan",
+//     lastname : "yadav",
+//     age:100,
+//     location:"siwan",
+//     fullname: function(){
+//         return this.name + " "+ this.lastname;
+//     }
+
+// };
+// console.log(person);
+// console.log(person.fullname)
+// console.log(Array.from([1,2,[1,2,3],[100,40,50]]));
+const name = {
+    username:{
+        firtname:"rohit",
+        lastname:"kumar"
+    }
+}
+console.log(name.username.firtname,name.username.lastname);
+
+console.log(Array.of("rohit","kumar","yadav"));
